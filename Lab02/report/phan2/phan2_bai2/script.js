f3ee55e0a564a9227a03da4cc12da3f00f000bf9
khanhdq109 <khanh.daoquoc@hcmut.edu.vn>
2024-04-03 14:04:21 +0700

    Add Lab03 description

--- Lab02/report/phan2/phan2_bai2/script.js
+++ Lab02/report/phan2/phan2_bai2/script.js
@@ -42,3 +42,16 @@ function calculate() {
 function getAns() {
     appendToScreen(lastResult);
 }
+
+// Function to handle manual input by typing
+document.addEventListener('keydown', function(event) {
+    var key = event.key;
+    // Check if the pressed key is a valid calculator input
+    if (/[\d\.+\-*/^]/.test(key)) {
+        event.preventDefault(); // Prevent typing in the input field
+        appendToScreen(key); // Append the pressed key to the calculator screen
+    } else if (key === 'Enter') {
+        event.preventDefault(); // Prevent the default action of the Enter key (submitting a form)
+        calculate(); // Perform calculation when Enter key is pressed
+    }
+});
